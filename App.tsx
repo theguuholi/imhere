24abd7da5b617e082e53dafc45aaca438c99e4a2
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-02-12 11:11:17 -0300

    organizacao dos files

--- App.tsx
+++ App.tsx
@@ -1,28 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
+import Home from "./src/screens/Home";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>Event Name</Text>
-      <Text style={styles.eventDate}>Friday, 4th November . 2024</Text>
-    </View>
-  );
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#131016",
-    padding: 24,
-  },
-  eventName: {
-    color: "#fdfcfe",
-    fontSize: 24,
-    fontWeight: "bold",
-    marginTop: 48,
-  },
-  eventDate: {
-    color: "#6b6b6b",
-    fontSize: 16,
-  },
-});
