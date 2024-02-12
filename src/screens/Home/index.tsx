225a40f35bfac7c489b64aa3f1ecd4591b0195b3
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-02-12 11:24:32 -0300

    input

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import { styles } from "./styles";
 
 export default function Home() {
@@ -6,6 +6,11 @@ export default function Home() {
     <View style={styles.container}>
       <Text style={styles.eventName}>Event Name</Text>
       <Text style={styles.eventDate}>Friday, 4th November . 2024</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Guest Name"
+        placeholderTextColor="#6b6b6b"
+      />
     </View>
   );
 }
