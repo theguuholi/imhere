9a7eeadd627aecddbad2e7893564f23344d3932c
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-02-14 16:39:17 -0300

    status bar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
+import { StatusBar } from "expo-status-bar";
 import Home from "./src/screens/Home";
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar backgroundColor="transparent" translucent />
+      <Home />
+    </>
+  );
 }
